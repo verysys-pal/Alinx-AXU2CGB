fea42a95f78da05f38494c1da30ccb72b684307c
sysPAL <tedanipal@gmail.com>
2026-01-13 16:54:45 +0900

    ✏️

--- course_s1_FPGA/03_ram_test/ram_test.srcs/sources_1/new/ram_test.v
+++ course_s1_FPGA/03_ram_test/ram_test.srcs/sources_1/new/ram_test.v
@@ -1,57 +1,57 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 module ram_test(
-			input clk,		          	//25MHz时钟
-			input rst_n	             	//复位信号，低电平有效	
+			input clk,		          	//25MHz clock
+			input rst_n	             	//Reset signal, low active
 		);
 
 //-----------------------------------------------------------
-reg		[8:0]  		w_addr;	   		//RAM PORTA写地址
-reg		[15:0] 		w_data;	   		//RAM PORTA写数据
-reg 	      		wea;	    	//RAM PORTA使能
-reg		[8:0]  		r_addr;	  	 	//RAM PORTB读地址
-wire	[15:0] 		r_data;			//RAM PORTB读数据
+reg		[8:0]  		w_addr;	   		//RAM PORTA write address
+reg		[15:0] 		w_data;	   		//RAM PORTA write data
+reg 	      		wea;	    	//RAM PORTA write enable
+reg		[8:0]  		r_addr;	  	 	//RAM PORTB read address
+wire	[15:0] 		r_data;			//RAM PORTB read data
 
-//产生RAM PORTB读地址
+//Generate RAM PORT-B read address
 always @(posedge clk or negedge rst_n)
 begin
-  if(!rst_n) 
+  if(!rst_n)
 	r_addr <= 9'd0;
-  else if (|w_addr)			//w_addr位或，不等于0
+  else if (|w_addr)			//w_addr bits are not all 0, start reading data
     r_addr <= r_addr+1'b1;
   else
-	r_addr <= 9'd0;	
+	r_addr <= 9'd0;
 end
 
-//产生RAM PORTA写使能信号
+//Generate RAM PORT-A write enable signal
 always@(posedge clk or negedge rst_n)
-begin	
-  if(!rst_n) 
+begin
+  if(!rst_n)
   	  wea <= #1 1'b0;
-  else 
+  else
   begin
-     if(&w_addr) 			//w_addr的bit位全为1，共写入512个数据，写入完成
-        wea <= #1 1'b0;                 
-     else               
-        wea	<= #1 1'b1;        //ram写使能
-  end 
-end 
+     if(&w_addr) 			//w_addr bit bits are all 1, a total of 512 data is written, write completed
+        wea <= #1 1'b0;
+     else
+        wea	<= #1 1'b1;     //ram wirte enable valid
+  end
+end
 
-//产生RAM PORTA写入的地址及数据
+//Generate RAM PORT-A write address and write data
 always@(posedge clk or negedge rst_n)
-begin	
-  if(!rst_n) 
+begin
+  if(!rst_n)
   begin
 	  w_addr <= 9'd0;
 	  w_data <= 16'd1;
   end
-  else 
+  else
   begin
-     if(wea) 					//ram写使能有效
-	 begin        
-		if (&w_addr)			//w_addr的bit位全为1，共写入512个数据，写入完成
+     if(wea) 					//ram write enable valid
+	 begin
+		if (&w_addr)			//w_addr bits are all 1, a total of 512 data is written, wirite completed
 		begin
-			w_addr <= w_addr ;	//将地址和数据的值保持住，只写一次RAM
+			w_addr <= w_addr ;	//keep address unchanged, write RAM only once
 			w_data <= w_data ;
 		end
 		else
@@ -60,11 +60,11 @@ begin
 			w_data <= w_data + 1'b1;
 		end
 	 end
-  end 
-end 
+  end
+end
 
 //-----------------------------------------------------------
-//实例化RAM	
+// Instantiate RAM IP core
 ram_ip ram_ip_inst (
   .clka      (clk          ),     // input clka
   .wea       (wea          ),     // input [0 : 0] wea
@@ -75,12 +75,12 @@ ram_ip ram_ip_inst (
   .doutb     (r_data       )      // output [15 : 0] doutb
 );
 
-//实例化ila逻辑分析仪
+// Instantiate ILA logic analyzer
 ila_0 ila_0_inst (
-	.clk	(clk	), 
-	.probe0	(r_data	), 
-	.probe1	(r_addr	) 
+	.clk	(clk	),
+	.probe0	(r_data	),
+	.probe1	(r_addr	)
 );
 
-	
+
 endmodule
